283e6ccbce945e9745052663ba28b5532e87e29a
Francisco <franciscols2011@gmail.com>
2024-09-13 14:50:30 -0300

    Se agregó la parte visual para filtrar productos por precio, de menor a mayor y viceversa, así como por orden alfabético de la A a la Z y viceversa.

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,9 +1,46 @@
+import { ArrowUpDownIcon } from "lucide-react";
+import { DropdownMenuTrigger } from "/components/ui/dropdown-menu";
+import { DropdownMenu } from "/components/ui/dropdown-menu";
 import ProductFilter from "/src/components/shopping-view/filter";
+import { Button } from "/src/components/ui/button";
+import { DropdownMenuContent } from "/components/ui/dropdown-menu";
+import { DropdownMenuRadioGroup } from "/components/ui/dropdown-menu";
+import { sortOptions } from "/src/config";
+import { DropdownMenuRadioItem } from "/components/ui/dropdown-menu";
 
 function ShoppingListing() {
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
 			<ProductFilter />
+			<div className="bg-background w-full rounded-lg shadow-sm">
+				<div className="p-4 border-b flex items-center justify-between">
+					<h2 className="text-lg font-extrabold">Products</h2>
+					<div className="flex items-center gap-3">
+						<span className="text-muted-foreground">10 Productos</span>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button
+									variant="outline"
+									size="sm"
+									className="flex items-center gap-1"
+								>
+									<ArrowUpDownIcon className="h-4 w-4" />
+									<span>Sort by</span>
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end" className="w-[200px]">
+								<DropdownMenuRadioGroup>
+									{sortOptions.map((sortItem) => (
+										<DropdownMenuRadioItem key={sortItem.id}>
+											{sortItem.label}
+										</DropdownMenuRadioItem>
+									))}
+								</DropdownMenuRadioGroup>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
